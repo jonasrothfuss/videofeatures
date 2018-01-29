a6343520fe4fdccdf8b5915463cba347c78377d5
ferreirafabio <f4bio.ferreira@gmail.com>
2018-01-29 19:49:44 +0100

    Armaer Datset Provider skeleton

--- DatasetProvider/ArmarDatasetProvider.py
+++ DatasetProvider/ArmarDatasetProvider.py
@@ -1,33 +1,37 @@
-from DatasetProvider import VideoDataset, Video
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from FeatureExtractor import VGG, SIFTFeatures
+from gulpio import GulpDirectory
+from gulpio.transforms import CenterCrop, ComposeVideo, Scale
+from gulpio.dataset import GulpVideoDataset
+from gulpio.loader import DataLoader
+
+#TODO set gulp paths
+
+TRAIN_GULP_DIR = ''
+VALID_GULP_DIR = ''
+
+class ArmarDataset:
+
+  def __init__(self, batch_size=20, n_frames=20):
+
+    transforms = ComposeVideo([CenterCrop(128), Scale((224, 224))])
+    self.n_frames = n_frames
+
+    self.train_dataset = GulpVideoDataset(TRAIN_GULP_DIR, 20, 1, False, transform=transforms)
+    self.train_loader = DataLoader(self.train_dataset, batch_size=10, shuffle=False, num_workers=8, drop_last=True)
+
+    self.val_dataset = GulpVideoDataset(VALID_GULP_DIR, n_frames, 1, False, transform=transforms)
+    self.val_loader = DataLoader(self.val_dataset, batch_size=batch_size, shuffle=False, num_workers=8, drop_last=True)
 
 
-class ArmarDataset(VideoDataset):
-
-  def __init__(self, root_dir):
-    super(ArmarDataset, self).__init__(root_dir)
-
-
-
+  def getDataLoader(self, train=False):
+    if train:
+      return self.train_loader
+    else:
+      return self.val_loader
 
 
 
+#TODO: write an adapter for the ARMAR dataset
 
+# /PDFData/rothfuss/data/ArmarExperiences
 
-
-
-if __name__ == "__main__":
-  root_dir = "/home/jonasrothfuss/Downloads/Armar_Experiences_Download/"
-  #ArmarDataset()
-  v = Video("/home/jonasrothfuss/Downloads/Armar_Experiences_Download/video_frames/29")
-  #video_array = v.getVideoAsArray(n_frames=5)
-  #vgg = VGG()
-  #feat = vgg.computeFeatures(video_array)
-  #print(feat.shape)
-  frames = v.frames
-  sift = SIFTFeatures()
-  a = sift.image_descriptors(frames[1])
-  print(a.shape)
-  print(a)
+# https://github.com/TwentyBN/GulpIO
--- DatasetProvider/__init__.py
+++ DatasetProvider/__init__.py
@@ -1,2 +1,3 @@
 from DatasetProvider.BaseDatasetProvider import VideoDataset, Video
 from DatasetProvider.ArmarDatasetProvider import ArmarDataset
+from DatasetProvider.TwentyBNDatasetProvider import TwentyBNDataset

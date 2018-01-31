fixed computation problem during gaussian estimaton in the Fisher vector computation pipeline, load and store fisher vectors

FeatureProcessing/FisherVector.py
@@ -62,7 +62,7 @@ class FisherVectorGMM:
     :param verbose - boolean that controls the verbosity
     :return: fitted Fisher vector object
     """
-    n_kernel_choices = [20, 40, 60, 100, 200]
+    n_kernel_choices = [20, 40, 60, 100, 150]
     bic_scores = []
     for n_kernels in n_kernel_choices:
       self.n_kernels = n_kernels
@@ -83,10 +83,10 @@ class FisherVectorGMM:
 
   def predict(self, X, normalized=True):
     """
-      Computes Fisher Vectors of provided X
-      :param X: features - ndarray of shape (n_videos, n_frames, n_features, n_feature_dim)
-      :param normalized: boolean that indicated whether the fisher vectors shall be normalized --> improved fisher vector
-      :returns fv: fisher vectors - ndarray of shape (n_videos, n_frames, 2*n_kernels, n_feature_dim)
+    Computes Fisher Vectors of provided X
+    :param X: features - ndarray of shape (n_videos, n_frames, n_features, n_feature_dim)
+    :param normalized: boolean that indicated whether the fisher vectors shall be normalized --> improved fisher vector
+    :returns fv: fisher vectors - ndarray of shape (n_videos, n_frames, 2*n_kernels, n_feature_dim)
     """
     assert self.fitted, "Model (GMM) must be fitted"
     assert self.feature_dim == X.shape[-1], "Features must have same dimensionality as fitted GMM"
@@ -98,8 +98,24 @@ class FisherVectorGMM:
     X_matrix = X.reshape(-1, X.shape[-1])
 
     #likelihood ratio
-    gaussians = [multivariate_normal(mean=self.means[k], cov=self.covars[k]) for k in range(self.n_kernels)]
-    likelihood = np.vstack([g.pdf(X_matrix) for g in gaussians]).T
+    #gaussians = [multivariate_normal(mean=self.means[k], cov=self.covars[k]) for k in range(self.n_kernels)]
+    #likelihood = np.vstack([multivariate_normal.pdf(X_matrix / 10**7, mean=self.means[k] / 10**7, cov=self.covars[k]/10**7) for k in range(self.n_kernels)]).T
+
+    gaussians = []
+    for k in range(self.n_kernels):
+      g = GaussianMixture(n_components=1, covariance_type='diag')
+      g._fitted = True
+      g.means_ = self.means[k].reshape((1,-1))
+      g.weights_ = np.asarray([1.])
+      g.covariances_ = np.diag(self.covars[k]).reshape((1,-1))
+
+      g.precisions_ = np.ones(g.covariances_.shape) / g.covariances_ # inverse of diagonal cov matrix is 1/cov of diagonals
+      g.precisions_cholesky_ = np.sqrt(g.precisions_) # since in the diagonal case the diag elements are squared for cholesky, we can get it by square rooting
+
+      gaussians.append(g)
+
+
+    likelihood = np.vstack([g.predict_proba(X_matrix) for g in gaussians]).T
     likelihood_ratio = likelihood / likelihood.sum(axis=-1)[:, None]
     likelihood_ratio = likelihood_ratio.reshape(X.shape[0], X.shape[1], self.n_kernels) #(n_images, n_features, n_kernels)
 

Pipeline.py
@@ -103,7 +103,8 @@ def loadFisherVectorGMM(pickle_path):
   assert isinstance(fv_gmm, FisherVectorGMM)
   return fv_gmm
 
-def computeFisherVectors(features, fv_gmm, normalized=True, pickle_path=None):
+
+def computeFisherVectors(features, labels, fv_gmm, normalized=True, pickle_path=None):
   """
   :param features: features as ndarray of shape (n_videos, n_frames, n_descriptors_per_image, n_dim_descriptor)
   :param fv_gmm: fitted FisherVectorGMM instance
@@ -111,17 +112,19 @@ def computeFisherVectors(features, fv_gmm, normalized=True, pickle_path=None):
   :return: fisher vectors - ndarray of shape (n_videos, n_frames, 2*n_kernels, n_feature_dim)
   """
   assert isinstance(fv_gmm, FisherVectorGMM)
-  assert features
+  assert isinstance(features, np.ndarray)
+  assert features.ndim == 4
 
   if not pickle_path:
     pickle_path = os.path.join(FeatureDumpsDir, "gmm_fisher_vectors" + ".pickle" )
 
   fv = fv_gmm.predict(features, normalized=normalized)
-  pd.to_pickle(fv, pickle_path)
-  print("Fisher vectors pickled to ", pickle_path)
 
-  return fv
+  df = pd.DataFrame(data={'labels': labels, 'features': np.vsplit(fv, features.shape[0])})
+  print('Dumped feature dataframe to', pickle_path)
+  df.to_pickle(pickle_path)
 
+  return df
 
 
 
@@ -158,15 +161,13 @@ def main():
   #  extractFeatures(extractor_type=extractor, dataset='20bn_val')
 
 
-  features, labels = extractFeatures(extractor_type='vgg_fc2', dataset='20bn_val')
-  fv_gmm = trainFisherVectorGMM(features)
-  fisher_vectors = computeFisherVectors(features, fv_gmm)
-
-
-  #features = loadFeaturesForTraining(feature_df_path='"/common/homes/students/rothfuss/Desktop/SURFfeatures_10_20bn_valid.pickle"')
+  #features, labels = extractFeatures(extractor_type='vgg_fc1', dataset='20bn_val')
+  features, labels = loadFeatures(feature_df_path='./DataDumps/Features/vgg_fc1_20bn_val')
+  fv_gmm = trainFisherVectorGMM(features, by_bic=False)
+  fisher_vectors = computeFisherVectors(features[:100], labels[:100], fv_gmm)
+  fisher_df = loadFisherVectors("./DataDumps/Features/gmm_fisher_vectors.pickle")
+  print(fisher_df)
   #features = loadFeatures(feature_df_path=os.path.join(FeatureDumpsDir, 'surf_20bn_val'))
-
-  #print(features)
 
   #loadFisherVectors(pickle_path)
 
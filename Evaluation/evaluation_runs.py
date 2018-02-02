e48b32e9019ae5f9dcfd87234629619fdb6031bd
ferreirafabio <f4bio.ferreira@gmail.com>
2018-02-02 23:18:21 +0100

    try-catch blocks added, del memmap to close map

--- Evaluation/evaluation_runs.py
+++ Evaluation/evaluation_runs.py
@@ -3,6 +3,7 @@ import numpy as np
 from Evaluation import nearest_neighbor
 from Pipeline import evaluateMatching
 import Pipeline
+import traceback
 
 """ settings for eval runs """
 settings_DEM = [[False, None], [True, 50], [True, 100], [True, 200]] # use_pca, #n_pca_components
@@ -30,20 +31,38 @@ def run_eval_DEM_model(path_to_pickle):
                    label_column_name=LABEL_COLUMN_NAME, features_column_name=FEATURES_COLUMN_NAME)
     logger.info('Finished matching evaluation. Results: \n{}'.format(dict))
     dicts.append(dict)
-  print(dicts)
 
-def run_fisher_vector_resnet_model(path_to_features, path_to_labels):
-  features = np.load(path_to_features, mmap_mode='r')
 
-  labels = np.load(path_to_labels, mmap_mode='r')
-  print(features.shape, labels.shape)
+def run_fisher_vector_gmm_model(path_to_fv, features_type, model):
+  fv, labels = Pipeline.loadFisherVectors(path_to_fv)
+  logger.info(
+    'Starting to evaluate matching results. Data used: {} Features used: {} Model type: {}'.format(path_to_fv, features_type, model))
+  dict = evaluateMatching(feature_vectors=fv, labels=labels)
+  logger.info('Finished matching evaluation. Results: \n{}'.format(dict))
+  del fv # delete the memmap to close
+
 
 if __name__ == '__main__':
   # set up logging
-  logger = Pipeline.setup_logger(logfile_name='piplineRuns.log')
-  #run_eval_DEM_model("/PDFData/rothfuss/selected_trainings/8_20bn_gdl_optical_flow/valid_run/metadata_and_hidden_rep_df_08-09-17_17-00-24_valid.pickle")
-  #run_eval_DEM_model("/PDFData/rothfuss/selected_trainings/7_20bn_mse/valid_run_backup/metadata_and_hidden_rep_df_07-26-17_16-52-09_valid.pickle")
-  run_fisher_vector_resnet_model("/PDFData/rothfuss/fisher_vector/fv_resnet_20bn_val.npy", "/PDFData/rothfuss/fisher_vector/fv_resnet_20bn_val_labels.npy")
+  logger = Pipeline.setup_logger(logfile_name='pipelineRuns.log')
+
+  try:
+    run_fisher_vector_gmm_model(path_to_fv="/PDFData/rothfuss/fisher_vector/fv_resnet_20bn_val", features_type="ResNet",
+                                model="FisherVectorGMM50Comp")
+  except Exception as e:
+    logger.error(traceback.format_exc())
+
+  try:
+    run_eval_DEM_model("/PDFData/rothfuss/selected_trainings/8_20bn_gdl_optical_flow/valid_run/metadata_and_hidden_rep_df_08-09-17_17-00-24_valid.pickle")
+  except Exception as e:
+    logger.error(traceback.format_exc())
+
+  try:
+    run_eval_DEM_model("/PDFData/rothfuss/selected_trainings/7_20bn_mse/valid_run_backup/metadata_and_hidden_rep_df_07-26-17_16-52-09_valid.pickle")
+  except Exception as e:
+    logger.error(traceback.format_exc())
+
+
 
 
 
